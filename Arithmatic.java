0d3b4eee99b04cd35e996e807a1476fc4bff1613
Harshika2deshmukh <harshikadeshmukh2@gmail.com>
2026-01-26 11:07:43 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Arithmatic {
+public static void main(String args[])
+{
+    int a,b;
+    System.out.println("Enter two numbers");
+    Scanner obj= new Scanner(System.in);
+    a=obj.nextInt();
+    b=obj.nextInt();
+    System.out.println("ADDITION "+(a+b));
+    System.out.println("SUBSTRACTION "+(a-b));
+    System.out.println("Division "+(a/b));
+    System.out.println("Multiplication "+(a*b));
+    System.out.println("Reminder "+(a%b));
+}
+}
